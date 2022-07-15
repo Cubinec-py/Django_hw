ddab4aade5fbb0810f0f1a6f1502166e4d557179
Sasha Honcharenko <san4os2012@gmail.com>
2022-07-15 15:38:30 +0300

    HW 10

--- triangle/views.py
+++ triangle/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
 from triangle.forms import TriangleForm
+from triangle.forms import PersonForm
+from triangle.models import Person
+
+
+def index(request):
+    return render(request, 'triangle/index.html', {})
 
 
 def get_form(request):
@@ -18,5 +24,45 @@ def get_form(request):
         {
             'get_form': form,
             'hypotenuse': hypotenuse
+        }
+    )
+
+
+def post_person(request):
+    if request.method == 'POST':
+        person_form = PersonForm(data=request.POST)
+        if person_form.is_valid():
+            Person.objects.create(
+                first_name=person_form.cleaned_data['first_name'],
+                last_name=person_form.cleaned_data['last_name'],
+                email=person_form.cleaned_data['email']
+            )
+            return redirect("triangle:index")
+    else:
+        person_form = PersonForm()
+    return render(
+        request,
+        "triangle/person_form.html",
+        {
+            'person_form': person_form,
+        }
+    )
+
+
+def edit_person(request, pk):
+    obj = get_object_or_404(Person, pk=pk)
+    if request.method == 'POST':
+        person_form = PersonForm(data=request.POST, instance=obj)
+        if person_form.is_valid():
+            person_form.save()
+            return redirect("triangle:index")
+    else:
+        person_form = PersonForm(instance=obj)
+    return render(
+        request,
+        "triangle/edit_person_form.html",
+        {
+            'person_form': person_form,
+            'obj': obj,
         }
     )
