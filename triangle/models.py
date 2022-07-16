02498439a7611ce211c22dca89b68e088b48f661
Sasha Honcharenko <san4os2012@gmail.com>
2022-07-16 21:36:03 +0300

    HW 9

--- triangle/models.py
+++ triangle/models.py
@@ -8,3 +8,45 @@ class Person(models.Model):
 
     def __str__(self):
         return self.first_name
+
+
+class City(models.Model):
+    city_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.city_name)
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return str(self.product_name)
+
+
+class Purveyor(models.Model):
+    city_name = models.OneToOneField(
+        City,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+
+    purveyor_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.purveyor_name)
+
+
+class Client(models.Model):
+    city_name = models.ForeignKey(
+        City,
+        on_delete=models.CASCADE
+    )
+
+    product_name = models.ManyToManyField(Product)
+
+    client_name = models.CharField(max_length=100)
+    client_email = models.EmailField(max_length=100)
+
+    def __str__(self):
+        return str(self.client_name)
